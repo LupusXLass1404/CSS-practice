3437ab8d29719d89c2856303513deded9b3c93e1
LupusXLass1404 <LxL@noreply.github.com>
2026-01-19 01:26:28 +0800

    maple 背包完成

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,11 @@
 /** @type { import('stylelint').Config } */
 export default {
-  extends: ['stylelint-config-clean-order'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-clean-order'
+  ],
+  // plugins: ['stylelint-selector-bem-pattern'],
+  rules: {
+    "selector-class-pattern": null,
+  }
 }
